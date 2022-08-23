9e91a3c135b8abe5ad8fa7845a7c2ce6c4c29010
Agustin Rojas <agus.rojasmolina@gmail.com>
2022-08-22 23:03:39 -0400

    added local storage

--- js/fotoTienda.js
+++ js/fotoTienda.js
@@ -1,5 +1,6 @@
 const productosContainer = document.querySelector('.store-section__cards')
 const btnBackTienda = document.querySelector('.store-section__back-btn')
+const carrito = document.querySelector('.store-section__cart')
 
 // localStorage
 let cart
@@ -20,16 +21,21 @@ btnBackTienda.addEventListener('click', function(){
     apartSection.style.display = 'none'
     cardsSection.style.display = 'flex'
     productSections.style.display = 'block'
-    verCarrito.style.display = 'block'
+    verCarritoBtn.style.display = 'block'
+    carrito.style.display = 'none'
 })
 
 // SHOW CART
-const verCarrito = document.querySelector('#store-section__cart-btn')
+const verCarritoBtn = document.querySelector('#store-section__cart-btn')
 const productSections = document.querySelector('.store-section__products')
-verCarrito.addEventListener('click', function(){
-    verCarrito.style.display = 'none'
+verCarritoBtn.addEventListener('click', function(){
+    verCarritoBtn.style.display = 'none'
+    carrito.style.display = 'flex'
+    carrito.style.flexDirection = 'column'
     productSections.style.display = 'none'
     btnBackTienda.style.display = 'block'
+    //
+
 })
 
 // FETCH FOTOPRODUCTOS
@@ -77,7 +83,7 @@ async function showProduct (){
     const btnVerProducto = document.querySelectorAll('.store-section__card')
     const apartSection = document.querySelector('.apart-section')
     const cardsSection = document.querySelector('.store-section__cards')
-    
+    carrito.style.display = 'none'
     let i = 0
     let length = btnVerProducto.length
     for(i; i < length; i++) {
@@ -99,35 +105,142 @@ async function showProduct (){
                             <div class="apart-section__data">
                                 <p class="apart-section__description">${productoEncontrado.descripcion}</p>
                             </div>
-                            <form>
+                            <form id="form">
                                 <label for="size">Seleccione el tamaño</label>
                                 <select class="apart-section__select" name="size" id="size">
-                                    <option value="30x40cm">${productoEncontrado.tamaño[0]}</option>
-                                    <option value="30x40cm">${productoEncontrado.tamaño[1]}</option>
-                                    <option value="30x40cm">${productoEncontrado.tamaño[2]}</option>
+                                    <option value="${productoEncontrado.tamaño[0]}">${productoEncontrado.tamaño[0]}</option>
+                                    <option value="${productoEncontrado.tamaño[1]}">${productoEncontrado.tamaño[1]}</option>
+                                    <option value="${productoEncontrado.tamaño[2]}">${productoEncontrado.tamaño[2]}</option>
                                 </select>
                                 <label for="marco">y el tipo de marco</label>
                                 <select class="apart-section__select" name="marco" id="marco">
-                                    <option value="30x40cm">${productoEncontrado.enmarcado[0]}</option>
-                                    <option value="30x40cm">${productoEncontrado.enmarcado[1]}</option>
-                                    <option value="30x40cm">${productoEncontrado.enmarcado[2]}</option>
+                                    <option value="${productoEncontrado.enmarcado[0]}">${productoEncontrado.enmarcado[0]}</option>
+                                    <option value="${productoEncontrado.enmarcado[1]}">${productoEncontrado.enmarcado[1]}</option>
+                                    <option value="${productoEncontrado.enmarcado[2]}">${productoEncontrado.enmarcado[2]}</option>
                                 </select>
+                                <div>
+                                    <h2>
+                                    Precio total:
+                                    </h2>
+                                </div>
                                 <div class="apart-section__price">
-                                    <h2>Total: $20000</h2>
+                                    <h2 id="price">$${productoEncontrado.precio[0]}</h2>
                                 </div>
                                 <div class="apart-section__onclick">
-                                    <button class="btn apart-section__btn-add" type="button">Agregar al carrito</button>
+                                    <button class="btn apart-section__btn-add" type="button" onclick="addToCart(${productoEncontrado.id})">Agregar al carrito</button>
                                 </div>
                             </form>
-                            
-                            
                         </div>
                     </div>
                 `
+
+                // SHOW PRODUCT PRICE
+                const form = document.getElementById('form')
+                let precioProducto
+                form.addEventListener('click', function() {
+                    const selectSize = document.getElementById('size')
+                    const selectMarco = document.getElementById('marco')
+                    const sectionPrice = document.querySelector('.apart-section__price')
+                    let sizeUser = selectSize.value
+                    let marcoUser = selectMarco.value
+                    if (sizeUser == productoEncontrado.tamaño[0] && marcoUser == productoEncontrado.enmarcado[0]) {
+                        precioProducto = productoEncontrado.precio[0]
+                    }
+                    if (sizeUser == productoEncontrado.tamaño[0] && marcoUser == productoEncontrado.enmarcado[1]) {
+                        precioProducto = productoEncontrado.precio[1]
+                    }
+                    if (sizeUser == productoEncontrado.tamaño[0] && marcoUser == productoEncontrado.enmarcado[2]) {
+                        precioProducto = productoEncontrado.precio[2]
+                    }
+                    if (sizeUser == productoEncontrado.tamaño[1] && marcoUser == productoEncontrado.enmarcado[0]) {
+                        precioProducto = productoEncontrado.precio[3]
+                    }
+                    if (sizeUser == productoEncontrado.tamaño[1] && marcoUser == productoEncontrado.enmarcado[1]) {
+                        precioProducto = productoEncontrado.precio[4]
+                    }
+                    if (sizeUser == productoEncontrado.tamaño[1] && marcoUser == productoEncontrado.enmarcado[2]) {
+                        precioProducto = productoEncontrado.precio[5]
+                    }
+                    if (sizeUser == productoEncontrado.tamaño[2] && marcoUser == productoEncontrado.enmarcado[0]) {
+                        precioProducto = productoEncontrado.precio[6]
+                    }
+                    if (sizeUser == productoEncontrado.tamaño[2] && marcoUser == productoEncontrado.enmarcado[1]) {
+                        precioProducto = productoEncontrado.precio[7]
+                    }
+                    if (sizeUser == productoEncontrado.tamaño[2] && marcoUser == productoEncontrado.enmarcado[2]) {
+                        precioProducto = productoEncontrado.precio[8]
+                    }
+                    
+                    sectionPrice.innerHTML = `
+                    <h2>$${precioProducto}</h2>
+                    `
+                })
+                
             })
         })
     }
 }
 
+// ADD TO CART
+async function addToCart(id) {
+    fetch('../json/fotoProductos.json').then(response => response.json()).then(async productos => {
+        let arrayProducts = await productos
+    if(cart.some((item) => item.id === id)) { // si el producto que quiero agregar ya existe en el carrito:
+        const itemFound = arrayProducts.find((fotoProd) => fotoProd.id == id) // find para saber que producto le corresponde el id, osea que fotoProd.id sea igual a lo que dice idBoton
+        const enCarrito = cart.find((fotoProd) => fotoProd.id == itemFound.id)
+        const carritoFiltrado = cart.filter(fotoProd => fotoProd.id != enCarrito.id) // filtrado de productos para traer los productos que no están en el carrito.
+        cart = [...carritoFiltrado, {...enCarrito, cantidad: enCarrito.cantidad + 1}] //agregar todos los productos(propiedad por propiedad) menos el que yo encontré en el carrito. agregar la cantidad que yo tenía en el carrito del producto +
+        const {nombre, enStock} = itemFound
+        console.log(`El producto ${nombre} tiene ${enStock} unidades en stock`)
+    } else {
+        const itemFound = arrayProducts.find((producto) => producto.id === id )
+        cart.push({...itemFound, cantidad: 1})
+        const {nombre, enStock} = itemFound
+        console.log(`El producto ${nombre} tiene ${enStock} unidades en stock`)
+    }
+    updateCart()
+    //contarProductos()
+    })
+}
 
+//UPDATE CART
+function updateCart(){
+    //showCartItems()
+    localStorage.setItem('cart', JSON.stringify(cart)) //actualizar el localstorage
+    /* const vaciarCarrito = document.querySelector('.btn--vaciar--carrito')
+    vaciarCarrito.addEventListener('click', function(e){
+        e.preventDefault
+        contador = 0
+        contadorProductos.innerHTML = `
+                <p>${contador}</p>
+                `
+        borrarCarrito()
+        contarProductos()
+    }) */
+}
 
+// MOSTRAR ITEMS AL GRID CARRITO
+const cartItems = document.querySelector('.cart__products')
+function showCartItems(){
+    cart.forEach((item) => {
+        cartItems.innerHTML += `
+            <div class="cart-item">
+                <div class="cart-item__image-box">
+                    <img src=${item.imagen} class="imagen--carrito" alt="">
+                </div>
+                <div class="cart-item__name-box">
+                    <h3 class="nombre--producto">${item.nombre}</h3>
+                </div>
+                <div class="cart-item__quantity-box">
+                    <p>${item.cantidad}</p>
+                </div>
+                <div class="cart-item__price-box">
+                    <p>$${(item.precio * item.cantidad).toLocaleString()}</p>
+                </div>
+                <div class="cart-item__remove">
+                    <button class="btn--quitar--producto" onclick="removeItemFromCart(${item.id})" >Quitar producto</button>
+                </div>
+            </div>
+            `
+})
+}
